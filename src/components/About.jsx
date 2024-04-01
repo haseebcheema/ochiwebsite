45c46b4471931124ec6d707f3d1fde2e9bc5323c
haseebcheema <ihaseebtariq1@gmail.com>
2024-04-01 21:51:47 +0400

    created about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,38 @@ import React from 'react';
 
 function About() {
   return (
-    <div className='w-full bg-[#CDEA68] p-12'>
-      <h1 className='text-[3.8vw] w-11/12 leading-none tracking-tight text-[#212121] font-["Neue Montreal"]'>
-        Ochi is a strategic partner for fast-grow­ing tech businesses that need to <span className='border-b-[3px] border-[#212121] cursor-pointer'>raise funds</span>, <span className='border-b-[3px] border-[#212121] cursor-pointer'>sell products</span>, <span className='border-b-[3px] border-[#212121] cursor-pointer'>explain complex ideas</span>, and <span className='border-b-[3px] border-[#212121] cursor-pointer'>hire great people</span>.</h1>
+    <div className='w-full bg-[#CDEA68] py-20 rounded-[1vw]'>
+      <h1 className='text-[3.8vw] px-12 w-11/12 leading-none tracking-tight text-[#212121] font-["Neue Montreal"]'>
+        Ochi is a strategic partner for fast-grow­ing tech businesses that need to <span className='border-b-[3px] border-[#212121] cursor-pointer'>raise funds</span>, <span className='border-b-[3px] border-[#212121] cursor-pointer'>sell products</span>, <span className='border-b-[3px] border-[#212121] cursor-pointer'>explain complex ideas</span>, and <span className='border-b-[3px] border-[#212121] cursor-pointer'>hire great people</span>.
+      </h1>
+      <hr className='mt-14 border-[#99AD52]'/>
+      <div className='flex justify-between px-12 py-6 text-[1.1vw] text-[#3B3F2B]'>
+        <div className='flex gap-[22vw]'>
+            <h5 className='w-80'>What you can expect:</h5>
+            <div className='w-72 leading-6'>
+                <p>We create tailored presentations to help you persuade your colleagues, clients, or investors. Whether it’s live or digital, delivered for one or a hundred people.</p>
+                <br />
+                <p>We believe the mix of strategy and design (with a bit of coffee) is what makes your message clear, convincing, and captivating.</p>
+            </div>
+        </div>
+        <div className='flex flex-col'>
+            <h5 className='mb-4'>S:</h5>
+            {['Instagram', 'Behance', 'Facebook', 'LinkedIn'].map((item, index) => (
+                <a key={index} className='mb-2 underline underline-offset-[0.2vw] cursor-pointer'>{item}</a>
+            ))}
+        </div>
+      </div>
+      <hr className='mt-14 border-[#99AD52]'/>
+      <div className='flex justify-between pt-6 px-12'>
+        <div className='flex flex-col gap-4'>
+            <h1 className='text-[#212121] text-5xl'>Our approach:</h1>
+            <button type='button' className='flex gap-7 items-center w-fit rounded-full px-5 py-3 text-white bg-[#212121] text-[1vw] border border-[#3B3F2B] cursor-pointer hover:bg-black hover:text-white'>
+                READ MORE <div className='w-2 h-2 bg-white rounded-full'></div></button>
+        </div>
+        <div>
+            <img src="https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-663x469.jpg" className='rounded-lg' alt="" />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -24,11 +24,11 @@ function LandingPage() {
       <hr className="mt-20 border-[#B2B2B2]"/>
       <div className="start flex justify-between items-center w-full pl-12 pt-4 font-['Neue Montreal']">
         {["For public and private companies", "From the first pitch to IPO"].map((item, index) => (
-            <h2 key={index} className="text-[1.1vw] text-[#212121]"> {item} </h2>
+            <h2 key={index} className="text-[1.1vw] text-[#3B3F2B]"> {item} </h2>
         ))}
-        <div className="projects flex gap-1 items-center pr-10 text-[#212121]">
-            <button type="button" className="rounded-full px-2 py-1 text-[1.1vw] border border-[#212121] cursor-pointer hover:bg-[#212121] hover:text-white">START THE PROJECT</button>
-            <IoMdArrowForward className="border border-[#212121] rounded-full size-8 px-1 -rotate-45 flex justify-center items-center cursor-pointer hover:bg-[#212121] hover:text-white"/>
+        <div className="projects flex gap-1 items-center pr-10 text-[#3B3F2B]">
+            <button type="button" className="rounded-full px-2 py-1 text-[1.1vw] border border-[#3B3F2B] cursor-pointer hover:bg-[#212121] hover:text-white">START THE PROJECT</button>
+            <IoMdArrowForward className="border border-[#3B3F2B] rounded-full size-8 px-1 -rotate-45 flex justify-center items-center cursor-pointer hover:bg-[#212121] hover:text-white"/>
         </div>
       </div>
     </div>
