3dd62dc745d2e17c48f94f55e19bff03317ee5bb
haseebcheema <ihaseebtariq1@gmail.com>
2024-04-03 16:18:46 +0400

    added few animations

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -27,7 +27,7 @@ function Start() {
     })
 
     return (
-        <div className="relative w-full min-h-screen pb-28 bg-[#CDEA68]">
+        <div data-scroll data-scroll-section data-scroll-speed="-.1" className="relative w-full min-h-screen pb-28 bg-[#CDEA68]">
             <button className='relative w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")]'></button>
             <button className='flex gap-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                 <div className='flex items-center justify-center w-[12vw] h-[12vw] bg-[#F1F1F1] rounded-full'>
