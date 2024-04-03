222ef7c3f7c5bd29143bb4579aa54491bc07cdac
haseebcheema <ihaseebtariq1@gmail.com>
2024-04-03 15:14:12 +0400

    animates the marquee component

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,11 +1,12 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 
 function marquee() {
   return (
     <div className='w-full py-20 bg-[#004D43] rounded-t-[1vw]'>
-      <div className='text text-white border-t-[0.1vw] border-b-[1px] border-[#B2B2B2] flex gap-2 overflow-hidden whitespace-nowrap font-["Founders Grotesk X-Condensed"]'>
-        <h1 className='uppercase text-[18vw] font-bold tracking-tighter'>we are ochi</h1>
-        <h1 className='uppercase text-[18vw] font-bold tracking-tighter'>we are ochi</h1>
+      <div className='text text-white border-t-[0.1vw] border-b-[1px] border-[#B2B2B2] flex overflow-hidden whitespace-nowrap font-["Founders Grotesk X-Condensed"]'>
+        <motion.h1 initial={{x: "0"}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: "6"}} className='uppercase text-[18vw] font-bold tracking-tighter pr-16'>we are ochi</motion.h1>
+        <motion.h1 initial={{x: "0"}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: "6"}} className='uppercase text-[18vw] font-bold tracking-tighter pr-16'>we are ochi</motion.h1>
       </div>
     </div>
   )
